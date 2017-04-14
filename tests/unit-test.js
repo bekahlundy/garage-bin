0ba5c495705e87713cf59b111f258acb1d606f98
bekahlundy <lundyrebekah@gmail.com>
2017-04-13 18:27:11 -0600

    Pushing up really bad unit test

--- tests/unit-test.js
+++ tests/unit-test.js
@@ -1,11 +1,17 @@
-const chai = require('chai');
-const expect = chai.expect;
-const assert = require('chai').assert
-const {sortUp, sortDown } = require('../public/index.js');
+const expect = require('chai').expect;
+const should = require('chai').should;
+const jsdom = require('mocha-jsdom')
+const {sortUp, sortDown} = require('../public/index.js');
 
-describe('App', () => {
+describe('Unit Tests', () => {
+  jsdom()
 
+  before(() => {
+    $ = require('jquery')
+    fetch = require('fetch')
+  })
 
-  it('should have a sortUp method', () => {
-
+  it('should sortUp', () => {
+      expect(sortUp(list)).to.equal(1)
+    })
 })
