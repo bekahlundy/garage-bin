bf80fd67f1430e1c416878e800be39af748bdc1f
bekahlundy <lundyrebekah@gmail.com>
2017-04-13 19:06:31 -0600

    Add some mock unit tests since it is not working

--- tests/unit-test.js
+++ tests/unit-test.js
@@ -1,7 +1,9 @@
 const expect = require('chai').expect;
 const should = require('chai').should;
 const jsdom = require('mocha-jsdom')
-const {sortUp, sortDown} = require('../public/index.js');
+const {sortUp, sortDown, clearList, clearPopUp, countArrayLengths} = require('../public/index.js');
+
+// these tests ARE ALL mocked out and not working. I tried many different ways to get these to work, but could not figure it out. It was always an error that failed everything, not just one test. My server tests are all passing however
 
 describe('Unit Tests', () => {
   jsdom()
@@ -12,6 +14,39 @@ describe('Unit Tests', () => {
   })
 
   it('should sortUp', () => {
-      expect(sortUp(list)).to.equal(1)
-    })
+    list = {
+      'soccer', 'bike', 'toys'
+    }
+      expect(sortUp(list)).to.equal({'bike', 'soccer', 'toys'})
+  })
+
+  it('should sortDown', () => {
+    list = {
+      'soccer', 'bike', 'toys'
+    }
+      expect(sortUp()).to.equal({'toys', 'soccer', 'bike'})
+  })
+
+  it('should clearList', () => {
+    itemlist = {
+      'soccer', 'bike', 'toys'
+    }
+      expect(clearList(itemList)).to.equal('')
+  })
+
+  it('should clearPopUp', () => {
+    info = {
+      'soccer', 'bike', 'toys'
+    }
+      expect(clearList(info)).to.equal('')
+  })
+
+  it('should countArrayLengths', () => {
+        sparklingArr = ['sparkling']
+        dustyArr = ['dusty']
+        rancidArr = ['rancid']
+
+      expect(countArrayLengths(sparklingArr, dustyArr, rancidArr)).to.equal(3)
+  })
+
 })
